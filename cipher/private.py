6f477aad684b283485de21df8e9d10f7fdde8bf0
Ms. Persaud <85592940+learncs0@users.noreply.github.com>
2022-04-30 23:09:51 +0000

    completed hw async MEM

---
+++ cipher/private.py
@@ -0,0 +1,52 @@
+#Mamudu, Eduardo and Michele worked together
+
+def encrypt(text, s):
+    result = ""
+    # traverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+        # Encrypt uppercase characters in plain text
+        if (char.isupper()):
+            #convert the letter into unicode
+            #make A unicode 0 by subtracting 65
+            #add shift
+            #allow wraparound by mod 26
+            #make A's unicode 65 again by adding 65
+            #convert back from unicode to letter
+            result += chr((ord(char) - 65 + s) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) - 97 + s) % 26 + 97)
+    return result
+
+
+#enter alphabetic text without spaces and a desired shift
+text = "Michele"
+s = 25
+
+print("Plain Text : " + text)
+print("Shift pattern : " + str(s))
+print("Cipher: " + encrypt(text, s))
+print()
+
+
+#Hw bellpepper version starts here
+def decrypt(text, s):
+    result = ""
+    # transverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+        if (char.isupper()):
+            result += chr((ord(char) - 65 - s) % 26 + 65)
+        else:
+            result += chr((ord(char) - 97 - s) % 26 + 97)
+    return result
+
+
+#Homework Bell Pepper Version: Write a function that decrypts a symmetric cipher with a given shift
+text = "Lhbgdkd"
+s = 25
+
+print("Plain Text : " + text)
+print("Shift pattern : " + str(s))
+print("Cipher: " + decrypt(text, s))
